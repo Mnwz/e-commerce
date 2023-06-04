bf64360e62815ec1041afe54efb60c84b1974e7f
Mnwz <marcosorlandork@gmail.com>
2023-06-03 23:19:23 -0300

    carousel 60% feito

--- src/script.js
+++ src/script.js
@@ -1,1 +1,18 @@
-console.log("teste");
+const radio = document.querySelector(".manual-btn");
+const cont = 1;
+
+document.querySelector("#radio1").checked = true;
+
+setInterval(() => {
+  proximaImg();
+}, 5000);
+
+function proximaImg() {
+  cont++;
+
+  if (cont > 3) {
+    cont = 1;
+  }
+
+  document.querySelector("#radio" + cont).checked = true;
+}
